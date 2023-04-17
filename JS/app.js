d0ce38eb7fbea17e5b043140fcce5209ac919c6c
Blackfyre1 <31318864+Blackfyre1@users.noreply.github.com>
2023-04-17 14:51:39 +0530

    Schema changes and cascade delete and insert

--- JS/app.js
+++ JS/app.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const { config } = require('process');
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb+srv://admin:xhBJxsjAn8oLKR6k@main.dttg1p4.mongodb.net/BDA_Labs");
-var loginemail;
+var loginemail = "";
 const secretKey = 'secret';
 
 var signUpSchema = new mongoose.Schema({
@@ -15,7 +15,8 @@ var signUpSchema = new mongoose.Schema({
     lname: String,
     phno: String,
     admin: Boolean,
-    password: String
+    password: String,
+    roll: String
    });
 
 var loginSchema = new mongoose.Schema({
@@ -23,51 +24,42 @@ var loginSchema = new mongoose.Schema({
     password: String
    });
 
-var callForTutorialSchema = new mongoose.Schema({
-    name: String,
-    id: String,
-    dept: String,
-    college: String,
-    yearofpassing: Number,
-    heading: String
-   });
+var userProjectSchema = new mongoose.Schema({
+    ID: String,
+    ArrayID: Array
+});
 
-var callForPaperSchema = new mongoose.Schema({
-    name: String,
-    dept: String,
-    college: String,
-    yearofpassing: Number,
-    Paper: String
-   });
+var userCourseSchema = new mongoose.Schema({
+    ID: String,
+    ArrayID: Array
+});
 
-var callForWorkshopSchema = new mongoose.Schema({
-    name: String,
-    id: String,
-    dept: String,
-    college: String,
-    yearofpassing: Number,
-    heading: String
-   });
+var userPublicationSchema = new mongoose.Schema({
+    userID: String,
+    publicationID: Array
+});
 
 var projectsSchema = new mongoose.Schema({
     name: String,
-    description: String
+    description: String,
+    instructor: String
 });
 
 var coursesSchema = new mongoose.Schema({
     course_name: String,
     instructor: String,
-    phd_ta: String,
-    mtech_ta: String,
+    ta: Array,
     number_of_students: String,
     no_of_it_student: String,
     mba_student: String,
-    publications_of_lab: String
    });
 
 var publicationSchema = new mongoose.Schema({
     name:String,
-    authors:[{type:String}],
+    supervisor:String,
+    courseName:String,
+    abstract:String,
+    authors:Array,
     pub_date:Date,
     publisher:String,
     location:String
@@ -79,28 +71,6 @@ var peopleSchema = new mongoose.Schema({
     course_name: String
 })
 
-var tutorialSchema = new mongoose.Schema({
-    heading: String,
-    abstract: String,
-    speaker: String
-   });
-
-var workshopSchema = new mongoose.Schema({
-    heading: String,
-    details: String,
-    link: String,
-   });
-
-var participateSchema = new mongoose.Schema({
-    id: String,
-    headingofworkshop: String,
-   });
-
-var takesSchema = new mongoose.Schema({
-    id: String,
-    headingoftutorial: String,
-   });
-
 module.exports={     
     fetchData:function(callback){
        var Login = Login.find({});
@@ -108,7 +78,6 @@ module.exports={
            if(err) throw err;
            return callback(data);
        })
-       
     }
 }
 const createToken = (payload) => {
@@ -121,17 +90,13 @@ const verifyToken = (token) => {
 var token = "";
 var Login = mongoose.model("Login", loginSchema,"Login");
 var SignUp = mongoose.model("SignUp", signUpSchema,"Login");
-var callForTutorial = mongoose.model("Call_For_Tutorial", callForTutorialSchema,"Call_For_Tutorial");
-var callForPaper = mongoose.model("Call_For_Paper", callForPaperSchema,"Call_For_Paper");
-var callForWorkshop = mongoose.model("Call_For_Workshop", callForWorkshopSchema,"Call_For_Workshop");
+var UserProject = mongoose.model("User_Project_Relation", userProjectSchema,"User_Project_Relation");
+var UserCourse = mongoose.model("User_Course_Relation", userCourseSchema,"User_Course_Relation");
+var UserPublication = mongoose.model("User_Publication_Relation", userPublicationSchema,"User_Publication_Relation");
 var courses = mongoose.model("Courses", coursesSchema,"Courses");
 var projects = mongoose.model("Projects", projectsSchema,"Projects");
-var people = mongoose.model("People", peopleSchema,"People");
-var tutorial = mongoose.model("Tutorials", tutorialSchema,"Tutorials");
-var workshop = mongoose.model("Workshops", workshopSchema,"Workshops");
-var participate = mongoose.model("Participate", participateSchema,"Participate");
-var takes = mongoose.model("Takes", takesSchema,"Takes");
 var publication = mongoose.model("Publications", publicationSchema,"Publications");
+var people = mongoose.model("People", peopleSchema,"People");
 
 
 var app = express();
@@ -142,59 +107,65 @@ app.use(bodyParser.json());
 app.use('/public', express.static(path.join(__dirname, "../public")));
 app.use('/Images', express.static(path.join(__dirname, "../Images")));
 
-app.post('/projectdelete', (req, res) => {
+async function DelOne(collection,res,query,token,dep)
+{
     try {
-        console.log(token);
-        const decoded = verifyToken(token);
-        console.log('Decoded:', decoded);
-        var myData = new projects(req.body);
-        projects.findOneAndDelete({name:req.body.name}).then(item => {
-        res.send("project deleted");
-        })
-        .catch(err => {
-        res.status(400).send("Unable to delete project");
-        });
+        verifyToken(token);
+        var myData = await collection.findOneAndDelete(query);
+        var doc = await dep.find({ArrayID:myData._id})
+        for(i = 0;i<doc.length;i++)
+        {
+            var x = doc[i].ArrayID;
+            var y = myData._id;
+            for(var j = 0;j<x.length;j++)
+            {
+                var a = JSON.stringify(x[j]);
+                var b = JSON.stringify(y);
+                console.log(a == b);
+                console.log(a);
+                if(a == b)
+                {
+                    if(j == x.length-1)
+                    {
+                        x.pop();
+                    }
+                    else
+                    {
+                        x.splice(j,1);
+                    }
+                    break;
+                }
+            }
+            dep.findOneAndDelete(doc[i]);
+            doc[i].ArrayID = x;
+            doc[i].save();
         }
-        catch (error) {
-            res.status(400).send('Error: Admin Login not detected.');
+        res.send("Deleted Successfully")
         }
-});
+    catch (error) {
+        res.status(400).send('Error: Admin Login not detected.');
+    }
+}
 
-app.post('/peopledelete', (req, res) => {
+function AddOne(collection,res,req,token)
+{
     try {
         console.log(token);
         const decoded = verifyToken(token);
         console.log('Decoded:', decoded);
-        var myData = new projects(req.body);
-        people.findOneAndDelete({"name":req.body.name,"post":req.body.post}).then(item => {
-        res.send("person deleted");
+        var myData = new collection(req.body);
+        myData.save()
+        .then(item => {
+        res.send(collection + " successful");
         })
         .catch(err => {
-        res.status(400).send("Unable to delete person");
+        res.status(400).send("unable to save to database");
         });
         }
         catch (error) {
             res.status(400).send('Error: Admin Login not detected.');
         }
-});
-
-app.post('/coursedelete', (req, res) => {
-    try {
-        console.log(token);
-        const decoded = verifyToken(token);
-        console.log('Decoded:', decoded);
-        var myData = new projects(req.body);
-        courses.findOneAndDelete({"course_name":req.body.course_name}).then(item => {
-        res.send("course deleted");
-        })
-        .catch(err => {
-        res.status(400).send("Unable to delete course");
-        });
-        }
-        catch (error) {
-            res.status(400).send('Error: Admin Login not detected.');
-        }
-});
+}
 
 app.post('/signup', function (req, res) {
     var myData = new SignUp(req.body);
@@ -249,18 +220,21 @@ app.post('/login_user', (req, res) => {
     })
 });
 
-app.post('/callForCoursesInput', function (req, res) {
+app.post('/coursesInput', function (req, res) {
     try {
         console.log(token);
         const decoded = verifyToken(token);
         console.log('Decoded:', decoded);
-        var myData = new Courses(req.body);
-        myData.save()
-        .then(item => {
-        res.send("Course added");
+        var myData = new courses(req.body);
+        const options = {
+            projection: { _id: 1}
+          };    
+        addUserDep(UserCourse,myData._id,req.body.ta,res,options);
+        myData.save().then(item => {
+        res.send("Course successful");
         })
         .catch(err => {
-        res.status(400).send("unable to save to database");
+            res.status(400).send("unable to save to database");
         });
         }
         catch (error) {
@@ -306,53 +280,23 @@ app.post('/callForWorkshopInput', function (req, res) {
         }
 });
 
-app.post('/callForPaperInput', function (req, res) {
-    try {
-        console.log(token);
-        const decoded = verifyToken(token);
-        console.log('Decoded:', decoded);
-        var myData = new callForPaper(req.body);
-        myData.save()
-        .then(item => {
-        res.send("Signup successful");
-        })
-        .catch(err => {
-        res.status(400).send("unable to save to database");
-        });
-        }
-        catch (error) {
-            res.status(400).send('Error: Admin Login not detected.');
-        }
-});
-
-app.post('/tutorials', function (req, res) {
-    try {
-        console.log(token);
-        const decoded = verifyToken(token);
-        console.log('Decoded:', decoded);
-        var myData = new tutorial(req.body);
-        myData.save()
-        .then(item => {
-        res.send("Signup successful");
-        })
-        .catch(err => {
-        res.status(400).send("unable to save to database");
-        });
-        }
-        catch (error) {
-            res.status(400).send('Error: Admin Login not detected.');
-        }
-});
-
 app.post('/publications', function (req, res) {
     try {
         console.log(token);
         const decoded = verifyToken(token);
         console.log('Decoded:', decoded);
         var myData = new publication(req.body);
-        myData.save()
-        .then(item => {
+        myData.save().then(item => {
         res.send("Publication successful");
+        for(var i = 0;i<req.body.authors.length;i++)
+        {
+            var x = Signup.findOne({email:req.body.authors[i]});
+            var y = Publication.findOne({mydata});
+            var userpubrel = userPublicationSchema({
+                userID: x.__id,
+                PublicationID: y.__id
+            })
+        }
         })
         .catch(err => {
         res.status(400).send("unable to save to database");
@@ -382,17 +326,6 @@ app.post('/workshops', function (req, res) {
         }
 });
 
-app.post('/participate', function (req, res) {
-    var myData = new participate(req.body);
-    myData.save()
-    .then(item => {
-    res.send("Signup successful");
-    })
-    .catch(err => {
-    res.status(400).send("unable to save to database");
-    });
-});
-
 app.post('/people', function (req, res) {
     try {
         console.log(token);
@@ -418,18 +351,61 @@ app.post('/projects', function (req, res) {
         const decoded = verifyToken(token);
         console.log('Decoded:', decoded);
         var myData = new projects(req.body);
-        myData.save()
-        .then(item => {
-        res.send("Data entered successfully");
+        const options = {
+            projection: { _id: 1}
+          };    
+        addUserDep(UserProject,myData._id,req.body.authors,res,options);
+        myData.save().then(item => {
+        res.send("Publication successful");
         })
         .catch(err => {
-        res.status(400).send("unable to save to database");
+            res.status(400).send("unable to save to database");
         });
-    }
-    catch (error) {
-        res.status(400).send('Error: Admin Login not detected.');
-    }
+        }
+        catch (error) {
+            res.status(400).send('Error: Admin Login not detected.');
+        }
 });
+
+async function addUserDep(Relation,myData,req,res,options)
+{
+    try{
+        for(var i = 0;i<req.length;i++)
+        {
+            var x = await SignUp.findOne({email:req[i]},options);
+            if(x == null)
+            {
+                res.status(200).send("User entry invalid. No user with email id" + req[i]);
+                return;
+            }
+        }
+        for(var i = 0;i<req.length;i++)
+        {
+            var x = await SignUp.findOne({email:req[i]},options);
+            console.log(x);
+            doc = await Relation.findOne({ID:x._id});
+            if(doc === null){
+                var userrel = Relation({
+                    ID: x._id,
+                    ArrayID: myData
+                })
+                userrel.save();
+                console.log(userrel);
+            }
+            else
+            {
+                doc.ArrayID.push(myData);
+                userrel = await Relation.findOneAndUpdate({ID:x._id},doc,{new: true});
+            }
+        }
+    }
+    catch(e)
+    {
+        console.log(e);
+        return false;
+    }
+    return true;
+}
 
 app.post('/courses', function (req, res) {
     try {
@@ -437,18 +413,26 @@ app.post('/courses', function (req, res) {
         const decoded = verifyToken(token);
         console.log('Decoded:', decoded);
         var myData = new courses(req.body);
-        myData.save()
-        .then(item => {
-        res.send("Data entered successfully");
-        })
-        
-        .catch(err => {
-        res.status(400).send("unable to save to database");
-        });
+        const options = {
+            projection: { _id: 1}
+            };    
+        if(addUserDep(Courses,myData._id,req,options))
+        {
+            myData.save().then(item => {
+            res.send("Course successful");
+            })
+            .catch(err => {
+                res.status(400).send("unable to save to database");
+            });
         }
-        catch (error) {
-            res.status(400).send('Error: Admin Login not detected.');
+        else
+        {
+            res.send("unknown error");
         }
+    }
+    catch (error) {
+        res.status(400).send('Error: Admin Login not detected.');
+    }
 });
 
 app.post('/takes', function (req, res) {
@@ -478,7 +462,7 @@ app.get('/login_user1', function (req, res) {
         const decoded = verifyToken(token);
         console.log('Decoded:', decoded);
         let x = path.join(__dirname,'../');
-        res.sendFile(x + '/user.html');
+        res.sendFile(x + '/user1.html');
       }
       catch (error) {
         res.status(400).send('Error: User Login not detected.');
@@ -490,12 +474,21 @@ app.get('/publicationInput', function (req, res) {
         console.log(token);
         const decoded = verifyToken(token);
         console.log('Decoded:', decoded);
-        let x = path.join(__dirname,'../');
-        res.sendFile(x + '/publication.html');
-      }
-      catch (error) {
+        var myData = new publication(req.body);
+        const options = {
+            projection: { _id: 1}
+            };    
+        addUserDep(UserPublication,myData._id,req,options)
+        myData.save().then(item => {
+        res.send("Publication successful");
+        })
+        .catch(err => {
+            res.status(400).send("unable to save to database");
+        });
+    }
+    catch (error) {
         res.status(400).send('Error: Admin Login not detected.');
-      }
+    }
 });
 
 app.get('/projectdelete', function (req, res) {
@@ -559,6 +552,21 @@ app.get('/courseInput', function (req, res) {
         res.status(400).send('Error: Admin Login not detected.');
       }
 });
+app.get('/publications', function (req, res) {
+    try {
+        console.log(token);
+        publication.find().then(( allUsers) => {
+            console.log(allUsers)
+            res.status(200).json(allUsers)
+        }).catch((e)=>{
+            console.log("Unable to load users!")
+            res.status(400).send(e)
+    })
+      }
+      catch (error) {
+        res.status(400).send('Error: Admin Login not detected.');
+      }
+});
 
 app.get('/peopleInput', function (req, res) {
     try {
@@ -617,6 +625,30 @@ app.get('/users', (req, res) => {
         res.status(400).send('Error: Admin Login not detected.');
       }
 });
+app.get('/user', (req, res) => {
+    try {
+        console.log(token);
+        const decoded = verifyToken(token);
+        console.log('Decoded:', decoded);
+        if(loginemail !==""){
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        SignUp.find({}).then(( allUsers) => {
+            console.log(allUsers)
+            res.status(200).json(allUsers)
+        }).catch((e)=>{
+            console.log("Unable to load users!")
+            res.status(400).send(e)
+        
+    })
+   }
+   else{
+    res.status(400).send("NO DATA")
+   }
+      } 
+      catch (error) {
+        res.status(400).send('Error: Admin Login not detected.');
+      }
+});
 app.get('/courses', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     courses.find().then(( allCourses) => {
@@ -638,16 +670,6 @@ app.get('/projects', (req, res) => {
     })
 });
 
-app.get('/tutorials', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    tutorial.find().then(( allTutorials) => {
-        console.log(allTutorials)
-        res.status(200).json(allTutorials)
-    }).catch((e)=>{
-        console.log("Unable to load tutorials!")
-        res.status(400).send(e)
-    })
-});
 app.get('/people', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     people.find().then(( allPeoples) => {
@@ -659,39 +681,32 @@ app.get('/people', (req, res) => {
     })
 });
 
-app.get('/workshop', (req, res) => {
+app.get('/logged',(req,res) =>{
     res.setHeader('Access-Control-Allow-Origin', '*');
-    workshop.find().then(( allWorkshops) => {
-        console.log(allWorkshops)
-        res.status(200).json(allWorkshops)
-    }).catch((e)=>{
-        console.log("Unable to load workshop!")
-        res.status(400).send(e)
-    })
-});
-
+    if(loginemail !== "" ){
+ const logJSON = {
+    loginemail 
+ }
+ res.status(200).json(logJSON);
+}
+else{
+    res.status(400).send("NO DATA");
+}
+}
+)
 app.delete('/projects/delete', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    projects.findOneAndDelete({name: req.body.name}).then((jt)=>{
-        console.log("Done it!")
-        res.send("DELTED SUCCESSFULLY")
-    }).catch((e)=>{console.log(e)});
+    DelOne(projects,res,{name:req.body.name},token,UserProject);
 });
 
 app.delete('/courses/delete', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    courses.findOneAndDelete({course_name: req.body.name}).then((jt)=>{
-        console.log("Done it!")
-        res.send("DELTED SUCCESSFULLY")
-    }).catch((e)=>{console.log(e)});
+    DelOne(courses,res,{course_name: req.body.name},token,UserCourse);
 });
 
-app.delete('/people/delete', (req, res) => {
+app.delete('/publication/delete', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    people.findOneAndDelete({name: req.body.name}).then((jt)=>{
-        console.log("Done it!")
-        res.send("DELTED SUCCESSFULLY")
-    }).catch((e)=>{console.log(e)});
+    DelOne(publication,res,{name: req.body.name},token,UserPublication);
 });
 
 app.listen(port, () => console.log(`This app is listening on port ${port}`));
